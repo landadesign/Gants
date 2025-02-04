3ca005c04e845ab283bde9bf5be6188f03716722
landadesign <shimizu@landadesign.jp>
2025-02-04 17:14:29 +0900

    Add files via upload

--- schedules_streamlit3.py
+++ schedules_streamlit3.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import japanize_matplotlib
-import jpholiday
 import datetime
 import io
 import time
